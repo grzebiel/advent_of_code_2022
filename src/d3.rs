6999321adfadab8241ae6e98460c02166020b329
grzebiel <grzebieluch@me.com>
2022-12-03 18:49:28 +0100

    refactor

--- src/d3.rs
+++ src/d3.rs
@@ -3,6 +3,14 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+pub fn item_to_priority(c: char) -> i64 {
+    match c {
+        c if c >= 'a' && c <= 'z' => c as i64 - 'a' as i64 + 1,
+        c if c >= 'A' && c <= 'Z' => c as i64 - 'A' as i64 + 27,
+        _ => panic!("unexpected input"),
+    }
+}
+
 pub fn p1() -> i64 {
     let file_reader = BufReader::new(File::open("input/d3.txt").unwrap());
     file_reader
@@ -17,11 +25,8 @@ pub fn p1() -> i64 {
         })
         .map(|(a, b)| {
             a.intersection(&b)
-                .map(|c| match c {
-                    c if c >= &'a' && c <= &'z' => *c as i64 - 'a' as i64 + 1,
-                    c if c >= &'A' && c <= &'Z' => *c as i64 - 'A' as i64 + 27,
-                    _ => panic!("unexpected input"),
-                })
+                .cloned()
+                .map(item_to_priority)
                 .sum::<i64>()
         })
         .sum()
@@ -32,23 +37,19 @@ pub fn p2() -> i64 {
     file_reader
         .lines()
         .map(|line| line.unwrap().chars().collect::<HashSet<char>>())
-        .into_iter()
         .chunks(3)
         .into_iter()
-        .map(|mut chunk| {
-            let a = chunk.next().unwrap();
-            let b = chunk.next().unwrap();
-            let c = chunk.next().unwrap();
-            a.intersection(&b)
+        .map(Vec::from_iter)
+        .map(|chunk| match &chunk[..] {
+            [a, b, c] => a
+                .intersection(&b)
                 .cloned()
                 .collect::<HashSet<_>>()
                 .intersection(&c)
-                .map(|c| match c {
-                    c if c >= &'a' && c <= &'z' => *c as i64 - 'a' as i64 + 1,
-                    c if c >= &'A' && c <= &'Z' => *c as i64 - 'A' as i64 + 27,
-                    _ => panic!("unexpected input"),
-                })
-                .sum::<i64>()
+                .cloned()
+                .map(item_to_priority)
+                .sum::<i64>(),
+            _ => panic!(""),
         })
         .sum()
 }
